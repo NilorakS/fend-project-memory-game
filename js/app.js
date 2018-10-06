dfd8f0796f0fca7a1402cc0edc988f3dd8e08829
NilorakS <karo.striegel@gmail.com>
2018-10-06 21:21:40 +0200

    implementation of click event listener functions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const cards = document.querySelectorAll('.card');
 
 /*
  * Display the cards on the page
@@ -9,6 +9,8 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -36,3 +38,104 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+cards.forEach(function(card) {
+    card.addEventListener('click', function onClick(event) {
+
+        turnOver(card);
+
+        if(twoCardsTurnedOver()) {
+            incrementMoveCounter();
+
+            if(currentPairMatches()) {
+                matchFound();
+                if(allMatchesFound()) {
+                    gameWon();
+                }
+            } else {
+                turnBackOverCurrentPair();
+            }
+        }
+    });
+});
+
+let currentPair = [];
+let moveCounter = document.querySelector('.moves');
+let matches = 0;
+
+function turnOver(card) {
+    if(!isTurnedOver(card)) {
+        addToCurrentPair(card);
+        displaySymbol(card);
+    }
+}
+
+function turnBackOverCurrentPair() {
+    setTimeout(function() {
+        currentPair.forEach(function(card) {
+            hideSymbol(card);
+        });
+        resetCurrentPair();
+    }, 1000);
+}
+
+function displaySymbol(card) {
+    card.classList.add('open', 'show');
+}
+
+function hideSymbol(card) {
+    card.classList.remove('open', 'show');
+}
+
+function displayMatch(card) {
+    card.classList.add('match');
+}
+
+function addToCurrentPair(card) {
+    currentPair.push(card);
+}
+
+function resetCurrentPair() {
+    currentPair.length = 0;
+}
+
+function isTurnedOver(card) {
+    return ((card.classList.contains('open') && card.classList.contains('show')));
+}
+
+function twoCardsTurnedOver() {
+    return currentPair.length == 2;
+}
+
+function currentPairMatches() {
+    return currentPair[0].firstElementChild.className == currentPair[1].firstElementChild.className;
+}
+
+function allMatchesFound() {
+    return matches == (cards.length/2);
+}
+
+function incrementMoveCounter() {
+    moveCounter.textContent++;
+} 
+
+function matchFound() {
+    currentPair.forEach(function(card) {
+        hideSymbol(card);
+        displayMatch(card);
+    });
+    resetCurrentPair();
+    matches++;
+}
+
+function gameWon() {
+    displayWinnerMessage();
+}
+
+function displayWinnerMessage() {
+    swal("Congratulations!", "You won with " + moveCounter.textContent + " moves!", "success", {
+        button: "Play again!",
+    // }).then((value) => {
+    //     resetGame();
+    })
+}
