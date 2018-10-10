27efdb112ed629714ad9ce1297962cc9c937387b
NilorakS <karo.striegel@gmail.com>
2018-10-10 16:48:17 +0200

    using ES6 declaration for shuffle variable

--- js/app.js
+++ js/app.js
@@ -136,7 +136,7 @@ function displayTime() {
 
 // randomly shuffles the cards (from http://stackoverflow.com/a/2450976)
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
